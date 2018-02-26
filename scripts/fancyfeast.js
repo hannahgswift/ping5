fe5a4571530aff170eaca21bac50f66932aaf048
Hannah Swift <h.graceswift@gmail.com>
2018-02-25 20:59:47 -0600

    Yet another dev portfolio redesign

---
+++ scripts/fancyfeast.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("#show-web").click(function(){
+    $(".hide").toggle(300);
+  });
+
+  $("#show-about").click(function(){
+    $(".about").toggle(300);
+  });
+});
